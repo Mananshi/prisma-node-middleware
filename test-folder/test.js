7e09c464b5737390bbc69f5f95cdb6e90a76c4cd
Mananshi <66679952+Mananshi@users.noreply.github.com>
2022-09-14 14:43:52 +0530

    v2.0.1 tested successfully

--- test-folder/test.js
+++ test-folder/test.js
@@ -1,5 +1,8 @@
-const sayHello = require("node-hello-name-test")
+const { sayHello, sayBye } = require("node-hello-name-test");
 let name = "John"
+
 
 console.log(sayHello())
 console.log(sayHello(name))
+console.log(sayBye())
+console.log(sayBye(name))
